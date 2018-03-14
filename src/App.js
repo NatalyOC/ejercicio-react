c85b5a3781dc277dab75068cb56578bca3e3c478
NatalyOC <noteroc@hotmail.com>
2018-03-14 10:05:04 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Section from './components/Section'
 
 const Hobbies=[{hobbie:'musica'}];
-//const ArtistaDefault = [{name : 'lulu', bio : 'programo, aprendo y desaprendo lo mas rápido que puedo, mientras menos tech mas sugar', url : 'lulu.jpg' , canciones : ['js', 'react', 'jquery']}];
+
 
 const App = () => (
   <div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header>
-        <h1> Hi</h1>
+    <h1> Hi</h1>
   </header>
 )
 export default Header;
